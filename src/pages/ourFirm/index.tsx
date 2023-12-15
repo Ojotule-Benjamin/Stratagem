44fd2be3a154d603a2bb09e40852bddc17cd03bb
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2023-12-15 16:22:05 +0100

    installed framer motion

--- src/pages/ourFirm/index.tsx
+++ src/pages/ourFirm/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import CustomHero from "../../components/CustomHero";
 import WelcomeMessage from "./WelcomeMessage";
+import OurPracticeArea from "../../components/ourPracticeArea/OurPracticeArea";
 
 const index = () => {
   return (
     <div>
       <CustomHero title="About The Firm" />
       <WelcomeMessage />
+      <OurPracticeArea />
     </div>
   );
 };
