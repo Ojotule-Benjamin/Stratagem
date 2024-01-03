a8650a0a2e5e66f2a115e538d6fdb00176f50670
Ojotule Benjamin Ajodo <benjamin.ojotule.ajodo@gmail.com>
2024-01-03 15:21:02 +0100

    update

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import CustomHero from "../../components/CustomHero";
 import Button from "../../components/Button";
+import telephoneIcon from "../../assets/svgs/telephoneIcon.svg";
+import mailIcon from "../../assets/svgs/mailIcon.svg";
+import locationIcon from "../../assets/svgs/locationIcon.svg";
+import contactImg from "../../assets/svgs/contactImg.svg";
 
 type dataProps = {
   name: string;
@@ -45,6 +49,71 @@ const Contact = () => {
   return (
     <div>
       <CustomHero title="CONTACT US" className=" bg-hero-blog" />
+      <div className="w-full px-4 md:px-14 lg:px-28 mb-12 lg:mb-24 flex flex-col lg:flex-row items-center justify-between gap-6 lg:gap-12 ">
+        <div className=" w-full h-[376px] flex items-center justify-center border-[#000] border-r-0 border-8">
+          <img src={contactImg} alt="" />
+        </div>
+        <div className=" w-full flex flex-col items-start justify-start gap-2">
+          <div className=" w-full flex items-center justify-start ">
+            <span className=" w-3 h-3 flex items-center justify-center bg-primary_color "></span>
+            <p className=" font-montserrat font-medium text-sm lg:text-lg text-[#37393F] pl-1">
+              Let's Talk
+            </p>
+          </div>
+          <h2 className=" font-montserrat font-bold text-2xl lg:text-4xl text-[#1F2732]">
+            Get In Touch With{" "}
+            <span className=" text-primary_color">Stratagem LP</span>
+          </h2>
+          <p className=" font-montserrat font-normal text-base lg:text-lg text-[#7D7D7D] pb-4 lg:pb-8">
+            We're here to connect and assist you in every way possible. Whether
+            you have inquiries about our legal services, wish to schedule a
+            consultation, explore potential partnerships, or wish to provide
+            feedback, please do not hesitate to get in touch with us. Our
+            dedicated team is prepared to offer you timely and personalized
+            assistance.
+          </p>
+          <div className="w-full flex flex-col items-start justify-start gap-4 lg:gap-7">
+            <div className=" w-full flex items-center justify-start gap-2">
+              <div className=" w-14 h-14 lg:w-[68px] lg:h-[68px] flex items-center justify-center rounded-full bg-primary_color">
+                <img src={telephoneIcon} alt="telephone icon" />
+              </div>
+              <div className=" font-montserrat font-normal text-xs lg:text-lg">
+                <p className=" text-[#7D7D7D]">Have any question?</p>
+                <a
+                  href="tel:+2348036362308"
+                  className=" font-lg text-[#37393F]"
+                >
+                  Call +234 8036362308
+                </a>
+              </div>
+            </div>
+            <div className=" w-full flex items-center justify-start gap-2 lg:gap-4">
+              <div className=" w-14 h-14 lg:w-[68px] lg:h-[68px] flex items-center justify-center rounded-full bg-primary_color">
+                <img src={mailIcon} alt="mail icon" />
+              </div>
+              <div className=" font-montserrat font-normal text-xs lg:text-lg">
+                <p className=" text-[#7D7D7D]">Write email</p>
+                <a
+                  href="mailto:info@stratagemlp.com"
+                  className=" font-lg text-[#37393F]"
+                >
+                  info@stratagemlp.com
+                </a>
+              </div>
+            </div>
+            <div className=" w-full flex items-center justify-start gap-2 lg:gap-4">
+              <div className=" w-14 h-14 lg:w-[68px] lg:h-[68px] flex items-center justify-center rounded-full bg-primary_color">
+                <img src={locationIcon} alt="location icon" />
+              </div>
+              <div className="h-auto font-montserrat font-normal text-xs lg:text-lg">
+                <p className=" text-[#7D7D7D]">Visit anytime</p>
+                <p>6, Umuahia Close,Off Emeka </p>
+                <p>Anyaoku Street, Area 11, Garki Abuja.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className=" w-full px-4 md:px-14 lg:px-28 pb-14 flex flex-col items-center justify-center">
         <div className=" w-full flex items-center justify-center mb-2 lg:mb-4">
